c2cf50a3ace752b9e80c96b4061a709ab8248380
Yi-Lin Guo <guovinn@gmail.com>
2025-02-14 16:16:16 +0800

    實作user mode
    Signed-off-by: Yi-Lin Guo <guovinn@gmail.com>

---
+++ hw13_user-mode/common.c
@@ -0,0 +1,73 @@
+#include "common.h"
+
+void putchar(char ch);
+
+void printf(const char *fmt, ...) {
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++; // 跳过 '%'
+            switch (*fmt) { // 读取下一个子富
+                case '\0': // 当 '%' 作为格式字符串的末尾
+                    putchar('%');
+                    goto end;
+                case '%': // 打印 '%'
+                    putchar('%');
+                    break;
+                case 's': { // 打印以 NULL 结尾的字符串
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+                case 'd': { // 以十进制打印整型
+                    int value = va_arg(vargs, int);
+                    if (value < 0) {
+                        putchar('-');
+                        value = -value;
+                    }
+
+                    int divisor = 1;
+                    while (value / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor > 0) {
+                        putchar('0' + value / divisor);
+                        value %= divisor;
+                        divisor /= 10;
+                    }
+
+                    break;
+                }
+                case 'x': { // 以十六进制打印整型
+                    int value = va_arg(vargs, int);
+                    for (int i = 7; i >= 0; i--) {
+                        int nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
